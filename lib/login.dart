3496fe697957ea091dd3fc4b8efea1c73055608c
codersanjay-inc <coder.sanjay19@gmail.com>
2022-11-29 19:49:50 +0530

    signup

--- lib/login.dart
+++ lib/login.dart
@@ -11,43 +11,88 @@ class _LoginUiState extends State<LoginUi> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
-      child: Scaffold(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(left: 35, top: 165),
-                child: const Text(
-                  "Dream2Day",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 33.0,
-                  ),
+          body: Stack(children: [
+            Container(
+              padding: const EdgeInsets.only(left: 35, top: 165),
+              child: const Text(
+                "बेरोज़गार",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 33.0,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.5,
-                    left: 35,
-                    right: 35),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        hintText: 'Email',
-                      ),
-                    ),
-                    TextField(),
-                  ],
-                ),
-              )
-            ],
-          )),
-    );
+            ),
+            SingleChildScrollView(
+                child: Container(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.5,
+                        left: 35,
+                        right: 35),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            hintText: 'Email',
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            hintText: 'Password',
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  fontSize: 27.0, fontWeight: FontWeight.w700),
+                            ),
+                            CircleAvatar(
+                              radius: 20,
+                              backgroundColor: const Color(0xff4c505b),
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.arrow_forward),
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            TextButton(
+                                onPressed: () {},
+                                child: const Text('SignUp',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                      decoration: TextDecoration.underline,
+                                      color: Color(0xff4c505b),
+                                    ))),
+                          ],
+                        )
+                      ],
+                    ))),
+          ]),
+        ));
+    // ignore: dead_code
   }
 }
